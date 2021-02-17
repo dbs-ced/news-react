93f3c5c5e564ae333347a21a796482522c271f03
Cedric Dubus <dbs.ced@gmail.com>
2021-02-17 11:15:44 +0100

    Rename homepage to top headlines news

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,11 +2,11 @@ import { Fragment } from 'react';
 import CountriesList from '../components/CountriesList';
 import NewsList from '../components/NewsList';
 
-export default function Home () {
+export default function Top () {
   return (
     <Fragment>
       <div className="container my-4">
-        <CountriesList />
+        {/* <CountriesList /> */}
 
         <NewsList source="top-headlines" />
       </div>
